4d25218f5ad11fca9c25e33e7d92ad26cbbe6240
Yaakov Magar <yaakov_magar@amat.com>
2022-07-04 20:36:03 +0300

    change on master

--- git_first.py
+++ git_first.py
@@ -1,10 +1,7 @@
 print(123)
-print(git"after first commit")
-print("after creating the branch")
-<<<<<<< HEAD
-print ("new line in branch-master")
-=======
-print ("new line igf branch-1")
-print ("new line in master only")
->>>>>>> new_branch
-abcd
+
+
+
+
+
+
